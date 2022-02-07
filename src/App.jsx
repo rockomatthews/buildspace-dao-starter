8faab3033b29a43e0607be1636eda0b8855962e8
rockomatthews <rob@robmatthe.ws>
2022-02-07 14:13:24 -0600

    Revoke main ETH account from having the right to mint

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import { useWeb3 } from "@3rdweb/hooks";
 import { ThirdwebSDK } from "@3rdweb/sdk";
 import { ethers } from "ethers";
+import { UnsupportedChainIdError } from "@web3-react/core";
 
 
 //instantiate the sdk on Rinkeby.
@@ -181,7 +182,20 @@ const memberList = useMemo(() => {
     });
   }, [address]);
 
+  if (error instanceof UnsupportedChainIdError ) {
+    return (
+      <div className="unsupported-network">
+        <h2>Please connect to Rinkeby</h2>
+        <p>
+          This dapp only works on the Rinkeby network, please switch networks
+          in your connected wallet.
+        </p>
+      </div>
+    );
+  }
   
+
+
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
   if (!address) {
